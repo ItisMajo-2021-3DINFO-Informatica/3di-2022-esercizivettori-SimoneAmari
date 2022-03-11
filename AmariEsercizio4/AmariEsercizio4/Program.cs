fcdf24b7870a11cf9908a9fa91e566c4e21a3ae8
SimoneAmari <simone.amari05@gmail.com>
2022-03-11 10:22:40 +0100

    Esercizio 3 e 4

---
+++ AmariEsercizio4/AmariEsercizio4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AmariEsercizio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeri = new int[100];
+            Random generatore = new Random();
+            int numeromin = 4000;
+            int numeromax = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                numeri[i] = generatore.Next(1000, 3001);
+                Console.WriteLine(numeri[i]);
+
+                if (numeromax < numeri[i])
+                {
+                    numeromax = numeri[i];
+                }
+                if (numeromin > numeri[i])
+                {
+                    numeromin = numeri[i];
+                }
+            }
+            Console.WriteLine("Il minimo è: " + numeromin);
+            Console.WriteLine("Il massimo è: " + numeromax);
+        }
+    }
+}
---
+++ AmariVettori3/AmariVettori3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AmariVettori3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeri = new int[30];
+            Random generatore = new Random();
+            int somma = 0;
+            for (int i = 0; i < 30; i++)
+            {
+                numeri[i] = generatore.Next(0, 51);
+                Console.WriteLine(numeri[i]);
+                somma = somma + numeri[i];
+            }
+
+            int media = somma / 30;
+            Console.WriteLine("La media è di: "+ media);
+        }
+    }
+}
