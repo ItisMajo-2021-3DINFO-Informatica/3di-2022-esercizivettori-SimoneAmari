7a4fd00ca602bc0131e38ebcb251aa29cb17b74f
SimoneAmari <simone.amari05@gmail.com>
2022-03-11 09:52:53 +0100

    Primi 2 esercizi

---
+++ AmariVettori2/AmariVettori2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AmariVettori2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeri = new int[100];
+            Random generatore = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                numeri[i] = generatore.Next(1, 101);
+                if(numeri[i]>=50)
+                {
+                    Console.WriteLine(numeri[i]);
+                }
+            }
+        }
+    }
+}
---
+++ AmariVettoriUno/AmariVettoriUno/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AmariVettoriUno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeri = new int[400];
+            Random generatore = new Random();
+            for (int i=0; i < 400; i++)
+            {
+                numeri[i] = generatore.Next(1, 101);
+                Console.WriteLine(numeri[i]);
+            }
+        }
+    }
+}
