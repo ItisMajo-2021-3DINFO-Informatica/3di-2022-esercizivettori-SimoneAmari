80b5bb65cca8e82777295cd0ef073db88ce8007d
SimoneAmari <simone.amari05@gmail.com>
2022-03-18 10:41:22 +0100

    Esercizio 6

---
+++ AmariVettori6/AmariVettori6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AmariVettori6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int dimensione = 8;
+            int[] tempi = new int[dimensione];
+            int tempomin = 999999999;
+            for (int i = 0; i<dimensione; i++)
+            {
+                Console.WriteLine("Digita il tempo in secondi");
+                tempi[i] = Convert.ToInt32(Console.ReadLine());
+                if (tempomin > tempi[i])
+                {
+                    tempomin = tempi[i];
+                }
+            }
+        }
+    }
+}
